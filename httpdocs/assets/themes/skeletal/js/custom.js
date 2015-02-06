a8b3e116c47b3d28c5a03e5c0a04660cbd66b49d
Brandon Sheppard <brandon.sheppard@gmail.com>
2015-02-06 13:47:35 +1000

    Removing AddThis and JCarousel by default

--- httpdocs/assets/themes/skeletal/js/custom.js
+++ httpdocs/assets/themes/skeletal/js/custom.js
@@ -125,26 +125,6 @@ $(document).ready(function() {
 
 });
 
-// JCarousel
-function mycarousel_initCallback(carousel)
-{
-	// Disable autoscrolling if the user clicks the prev or next button.
-	carousel.buttonNext.bind('click', function() {
-		carousel.startAuto(0);
-	});
-
-	carousel.buttonPrev.bind('click', function() {
-		carousel.startAuto(0);
-	});
-
-	// Pause autoscrolling if the user moves with the cursor over the clip.
-	carousel.clip.hover(function() {
-		carousel.stopAuto();
-	}, function() {
-		carousel.startAuto();
-	});
-};
-
 $(".btn-loads").click(function(){
 	$(this).button("loading");
 	var pendingbutton=this;
@@ -168,9 +148,15 @@ $(document).ready(function() {
 // Tooltip
 $('.tipsy').tooltip({trigger:'hover',placement:'bottom'});
 
-// AddThis configuration
-var addthis_config =
-{
-	ui_offset_top: -12,
-	services_compact: 'facebook,twitter,google_plusone_share,tumblr,linkedin'
+// Who needs AddThis?
+function windowPopup(url, width, height) {
+	// Calculate the position of the popup so
+	// it’s centered on the screen.
+	var left = (screen.width / 2) - (width / 2),
+		top = (screen.height / 2) - (height / 2);
+	window.open(url,"","menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=" + width + ",height=" + height + ",top=" + top + ",left=" + left);
 }
+$(".js-social-share").on("click", function(e) {
+	e.preventDefault();
+	windowPopup($(this).attr("href"), 500, 300);
+});
