c01b0fd3718a147a6ea3a29707a655683e698c8e
Brandon Sheppard <brandon.sheppard@gmail.com>
2016-01-05 20:43:13 +1000

    Updating bootstrap. Moving bootstrap into npm and whitelisting package.json
    because it is useful.

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,16 @@ var less = require('gulp-less');
 var path = require('path');
 
 gulp.task('less', function () {
-	gulp.src('./httpdocs/assets/themes/skeletal/css/app/app.less')
+	gulp.src('./httpdocs/assets/themes/**/css/app.less')
 	.pipe(less({
 		paths: [ path.join(__dirname, 'less', 'includes') ]
 	}))
-	.pipe(gulp.dest('./httpdocs/assets/themes/skeletal/css/'));
+	.pipe(gulp.dest('./httpdocs/assets/themes/'));
 });
 
 gulp.task('watch-less', function(){
 	// What to watch
-	gulp.watch('./httpdocs/assets/themes/skeletal/css/app/**/*.less', function(){
+	gulp.watch('./httpdocs/assets/themes/*/css/**/*.less', function(){
 		// What to run
 		gulp.run('less');
 	});
