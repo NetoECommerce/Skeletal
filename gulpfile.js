a999dc637f68ed221a45181f89c40212339a07f2
Curtis Tate Wilkinson <curtis@neto.com.au>
2016-09-09 11:19:33 +1000

    gulpfile.js modification to check less file is present
    Initial implementation of error handling

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
+var fs = require('fs');
 
 gulp.task('less', function () {
+	fs.stat('./src/css/app.less', function(err, stat) {
+		if(err != null) {
+			console.log('Error:' + err.code);
+		}
+	});
 	gulp.src('./src/css/app.less')
 	.pipe(less({
 		paths: [ path.join(__dirname, 'less', 'includes') ]
