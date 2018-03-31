d12740c1227a11c664db331228ba5313f9c86d19
Phil <phil@neto.com.au>
2018-03-31 17:38:01 +1000

    updated product templates classes

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('sass', function() {
 
 // Move the javascript files into our /src/js folder
 gulp.task('js', function() {
-	return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+	return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
 		.pipe(gulp.dest(config.JS))
 });
 
