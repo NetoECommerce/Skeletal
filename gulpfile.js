d8d7feb08a3d1d614d72b2eef363af8966a498f3
philcon93 <phil@neto.com.au>
2018-09-26 15:24:30 +1000

    updated to gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var path = require('path');
 var fs = require('fs');
-var gulp = require('gulp');
+var { task, src, dest, watch, series, parallel } = require('gulp');
 var postcss = require('gulp-postcss');
 var cssnext = require('postcss-cssnext');
 var sass = require('gulp-sass');
@@ -11,28 +11,30 @@ config.src = './src';
 config.JS = config.src + '/js';
 config.SCSS = config.src + '/scss';
 config.CSS = config.src + '/css';
+config.buildTasks = ['sass', 'js'];
 
 // Compile SASS into CSS
-gulp.task('sass', function() {
+task('sass', function() {
 	var plugins = [ cssnext ];
-	return gulp.src(config.SCSS +'/*.scss')
+	return src(config.SCSS +'/*.scss')
 		.pipe(sass())
 		.pipe(postcss(plugins))
-		.pipe(gulp.dest(config.CSS))
+		.pipe(dest(config.CSS))
 });
 
 // Move the javascript files into our /src/js folder
-gulp.task('js', function() {
-	return gulp.src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js'])
+task('js', function() {
+	return src(['node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js', 'node_modules/bootstrap/dist/js/bootstrap.min.js'])
 		.pipe(concat('vendor.js'))
-		.pipe(gulp.dest(config.JS))
+		.pipe(dest(config.JS))
 });
 
 // Watches scss files
-gulp.task('watch', ['sass'], function() {
-	gulp.watch([config.SCSS + '/*.scss', config.SCSS + '/_*.scss'], ['sass']);
-});
+task('watch', series('sass', function(done) {
+	watch([config.SCSS + '/*.scss', config.SCSS + '/_*.scss'], series('sass'));
+	done();
+}));
 
-gulp.task('build', ['sass', 'js']);
+task('build', series(config.buildTasks));
 
-gulp.task('default', ['watch']);
+task('default', series('watch'));
